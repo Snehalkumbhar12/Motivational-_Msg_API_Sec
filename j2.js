8586279bed480b2e18313f0c4821e72f6a3ee891
Snehalkumbhar12 <135451631+Snehalkumbhar12@users.noreply.github.com>
2023-06-05 18:20:47 +0530

    Add files via upload

---
+++ j2.js
@@ -0,0 +1,13 @@
+function gm()
+{
+		let msg = document.getElementById("msg");
+		let urladd = "https://api.quotable.io/random";
+		fetch(urladd)
+		.then(res=>res.json())
+		.then(data=>msg.innerHTML=data.author+ "==>" + data.content)
+		.catch(err=>msg.innerHTML=err);
+
+
+}
+	gm()
+	setInterval(gm,1000);
